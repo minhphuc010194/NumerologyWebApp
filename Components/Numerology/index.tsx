1a58803db17e9c36c11985cd1eed8f5cb06d0a20
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-09-12 23:33:13 +0700

    add state onChange input name

--- Components/Numerology/index.tsx
+++ Components/Numerology/index.tsx
@@ -1,12 +1,22 @@
-import { FC } from "react";
+import { FC, useDeferredValue, useState } from "react";
 import { Box, Heading, Input } from "../";
 import { Numerology as NumerologyTxt } from "../../Utils/constaints";
 
 export const Numerology: FC = () => {
+   const [name, setName] = useState<string>("");
+   const deferredName = useDeferredValue(name);
+
    return (
       <Box>
          <Heading textAlign="center">{NumerologyTxt}</Heading>
-         <Input placeholder="họ tên đầy đủ" />
+         <Box as="br" />
+         <Box textAlign="center">
+            <Input
+               placeholder="Nhập họ tên đầy đủ"
+               w={{ md: "50%", xs: "100%" }}
+               onChange={(e) => setName(e.target.value)}
+            />
+         </Box>
       </Box>
    );
 };
