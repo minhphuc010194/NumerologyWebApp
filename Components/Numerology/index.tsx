e66ffbcf8e1d0ef5443bdf693fe692871fa6ef7c
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-09-12 23:16:46 +0700

    add Input name

--- Components/Numerology/index.tsx
+++ Components/Numerology/index.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
-import { Box, Heading } from "../";
+import { Box, Heading, Input } from "../";
 import { Numerology as NumerologyTxt } from "../../Utils/constaints";
 
 export const Numerology: FC = () => {
    return (
       <Box>
          <Heading textAlign="center">{NumerologyTxt}</Heading>
+         <Input placeholder="họ tên đầy đủ" />
       </Box>
    );
 };
