99837ea4af43644271245dca8b8a6a8e172db86b
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-09-26 09:41:55 +0700

    fix missing calculator walks of life number

--- Functions/Numerology/sumAdjacent.ts
+++ Functions/Numerology/sumAdjacent.ts
@@ -1,16 +1,24 @@
 // cong 2 so lien ke
 export function sumAdjacent(
    strFirst: string | number,
-   strNext: string | number
+   strNext: string | number,
+   flagNumerology?: "walksOfLife"
 ): number {
    const sumDate = Number(strFirst) + Number(strNext);
    if (sumDate >= 10) {
+      if (
+         flagNumerology === "walksOfLife" &&
+         (sumDate === 10 || sumDate === 11 || sumDate === 22)
+      ) {
+         return sumDate;
+      }
       const strSumDate = String(sumDate).split("");
       const str1 = strSumDate[0];
       const str2 = strSumDate
          .splice(1, strSumDate.length - 1)
          .toString()
          .replace(/\,/g, "");
+
       return sumAdjacent(str1, str2);
    }
    return sumDate;
