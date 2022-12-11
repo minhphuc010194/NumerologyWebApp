e4c193e9ff0505872cd6d5de0e23fde4cd5a7866
Châu Minh Phúc <chauminhphuc1994it@gmail.com>
2022-12-11 21:09:36 +0700

    fix bug calculation index soul with 'Y' charactor in last index - special case

--- Functions/Numerology/getSoul.ts
+++ Functions/Numerology/getSoul.ts
@@ -2,17 +2,28 @@ import { DataVowels } from "../../data/mooks";
 import { getValueInAlphabets } from "./getValueInAlphabets";
 import { sumAdjacent } from "./sumAdjacent";
 
-export const getSoul = (strArray: string[]): number => {
-   const soulNumber = strArray.reduce(
-      (prevValue: number, currentValue: string) => {
-         const isVowel = DataVowels.indexOf(currentValue) >= 0;
-         if (isVowel) {
-            return prevValue + getValueInAlphabets(currentValue);
-         }
-         return prevValue;
-      },
-      0
-   );
+export const getSoul = (fullName: string): number => {
+   if (!fullName?.trim()) return 0;
+   const splitName = fullName.split(" ");
+   // console.log(splitName);
+   // const strArray = fullName.replace(/\s/g, "").split("");
+   let soulNumber = 0;
+   splitName.map((strs) => {
+      const strArray = strs.replace(/\s/g, "").split("");
+
+      soulNumber += strArray.reduce(
+         (prevValue: number, currentValue: string, index: number) => {
+            const isVowel =
+               DataVowels.indexOf(currentValue) >= 0 ||
+               (currentValue === "Y" && index === strArray.length - 1);
+            if (isVowel) {
+               return prevValue + getValueInAlphabets(currentValue);
+            }
+            return prevValue;
+         },
+         0
+      );
+   });
    const strSum = soulNumber.toString().split("");
    const str1 = strSum[0];
    const str2 = strSum.splice(1, strSum.length - 1).toString();
