71973d6387995fedfed525a9aed959a5ff77f6be
Minh Phuc <chauminhphuc1994it@gmail.com>
2026-04-10 20:53:20 +0700

    feat: add modal for detailed numerology metric display and update translations

--- components/Numerology/DisplayCard.tsx
+++ components/Numerology/DisplayCard.tsx
@@ -1,5 +1,5 @@
 import { FC, ReactNode } from "react";
-import { Box, HStack, Heading, Text } from "../";
+import { Box, Heading, Text } from "../";
 import { BoxProps } from "../../utils/types";
 
 type PropTypes = BoxProps & {
@@ -14,22 +14,35 @@ export const DisplayCard: FC<PropTypes> = ({
    ...rest
 }) => {
    return (
-      <HStack spacing={4}>
-         <Box p={3} shadow="base" borderWidth="1px" {...rest}>
-            <Heading fontSize="md">{title}</Heading>
-            <Text fontSize="small" color="gray.400">
+      <Box 
+         p={{ base: 2, md: 3 }} 
+         w="100%"
+         h="100%"
+         shadow="base" 
+         borderWidth="1px" 
+         display="flex"
+         flexDirection="column"
+         justifyContent="center"
+         alignItems="center"
+         {...rest}
+      >
+         <Heading fontSize={{ base: "xs", md: "sm" }} fontWeight={700} lineHeight="1.3" title={title}>
+            {title}
+         </Heading>
+         {name && (
+            <Text fontSize="xs" color="gray.400" mt={1}>
                {name}
             </Text>
-            {typeof content === 'string' || typeof content === 'number' ? (
-               <Text mt={2} fontSize="3xl" color="red.400" fontWeight={600}>
-                  {content}
-               </Text>
-            ) : (
-               <Box mt={2} fontSize="3xl" color="red.400" fontWeight={600}>
-                  {content}
-               </Box>
-            )}
-         </Box>
-      </HStack>
+         )}
+         {typeof content === 'string' || typeof content === 'number' ? (
+            <Text mt={{ base: 1, md: 2 }} fontSize={{ base: "2xl", md: "3xl" }} color="red.400" fontWeight={800}>
+               {content}
+            </Text>
+         ) : (
+            <Box mt={{ base: 1, md: 2 }} fontSize={{ base: "2xl", md: "3xl" }} color="red.400" fontWeight={800}>
+               {content}
+            </Box>
+         )}
+      </Box>
    );
 };
